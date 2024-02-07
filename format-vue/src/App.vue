fe53157ed4a5f464adf0944a27b3d3042c049481
DavideTarantino <davidetarantino2005@gmail.com>
2024-02-07 15:42:53 +0100

    nav-bar

--- format-vue/src/App.vue
+++ format-vue/src/App.vue
@@ -1,9 +1,15 @@
 <script>
+    import NavBar from './components/NavBar.vue'
 
+    export default {
+        components: {
+            NavBar
+        }
+    }
 </script>
 
 <template>
-
+    <NavBar/>
 </template>
 
 <style lang="scss">
---
+++ format-vue/src/components/NavBar.vue
@@ -0,0 +1,92 @@
+<script>
+    export default{
+        data(){
+            name: 'NavBar'
+            return{
+                cta: [
+                    {
+                        name: 'characters',
+                        current: false
+                    },
+                    {
+                        name: 'comics',
+                        current: true
+                    },
+                    {
+                        name: 'movies',
+                        current: false
+                    },
+                    {
+                        name: 'tv',
+                        current: false
+                    },
+                    {
+                        name: 'games',
+                        current: false
+                    },
+                    {
+                        name: 'collectibles',
+                        current: false
+                    },
+                    {
+                        name: 'videos',
+                        current: false
+                    },
+                    {
+                        name: 'fans',
+                        current: false
+                    },
+                    {
+                        name: 'news',
+                        current: false
+                    },
+                    {
+                        name: 'shop',
+                        current: false
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <main>
+        <figure>
+            <img src="../img/favicon.ico" alt="logo">
+        </figure>
+            <ul>
+                <li :class=" element.current ? 'active' : '' " v-for="(element,index) in cta">{{ element.name }}</li>
+            </ul>
+    </main>
+</template>
+
+<style lang="scss">
+    main{
+        display: flex;
+        justify-content: space-between;
+        width: 70%;
+        margin: 0 auto;
+        padding: 1%;
+        align-items: center;
+
+        img{
+            width: 150%;
+        }
+
+        ul{
+            display: flex;
+            gap: 30px;
+
+            li{
+                text-transform: uppercase;
+                font-family: sans-serif;
+                font-size: 80%;
+            }
+        }
+    }
+
+    .active{
+        color: blue;
+    }
+</style>
